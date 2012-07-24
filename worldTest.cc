56e3d6f9db6146e823ca9276c19b0c77e4b7b03a
2K <kkopiec_mound_eu_org>
2012-07-24 11:15:32 +0100

    World class added, worldTest.cc added - test file

---
+++ worldTest.cc
@@ -0,0 +1,14 @@
+#include "world.h"
+
+int main(int argc, char *argv[]){
+  World *w = new World(10,10);
+  w->flipOver(4,4);
+  w->flipOver(5,4);
+  w->flipOver(6,4);
+  w->flipOver(5,5);
+  for (int i =0 ;i < 11; i++){
+    w->evaluate();
+    std::cout << *w;
+  }
+  return 0;
+}
